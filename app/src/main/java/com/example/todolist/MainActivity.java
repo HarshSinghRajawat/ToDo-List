393ecfd5508c97d039c8a5918172e701dc07eb74
HarshSinghRajawat <harshsinghrajawat86@gmail.com>
2021-03-12 12:40:14 +0530

    Added Hint Dialog.

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.todolist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.content.ContentProvider;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -54,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        alertDialog();
 
 
     }
@@ -66,6 +68,26 @@ public class MainActivity extends AppCompatActivity {
         todoCursorAdapter adapter=new todoCursorAdapter(MainActivity.this,data);
         listView.setAdapter(adapter);
 
+    }
+    private void alertDialog() {
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setMessage("This is a Simple To Do list Application where:-\n\n 1. You can add your Tasks by Clicking on '+'.\n\n 2.You can Edit Your Existing Tasks by Touch and Holding Tasks.\n\n3.You Check your Tasks once they are completed.\n");
+        dialog.setTitle("Hint");
+        dialog.setPositiveButton("YES",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+
+                    }
+                });/*
+        dialog.setNegativeButton("cancel",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(),"cancel is clicked",Toast.LENGTH_LONG).show();
+            }
+        });*/
+        AlertDialog alertDialog=dialog.create();
+        alertDialog.show();
     }
 
 
